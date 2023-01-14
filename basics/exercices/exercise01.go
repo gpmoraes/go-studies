718d61b9f014b82c4d6ce98fa44ad8e872541ec4
Guilherme Moraes <moraes.guilhermep@gmail.com>
2023-01-14 13:22:46 -0300

    Comments

---
+++ basics/exercices/exercise01.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Time to practice:
+// 1- Create a new GO code file, set a package and add the main function
+func main()  {
+
+	// 2- Declare two new variables:
+	// - The number PI
+	pi := 3.14
+	// - Circle radius
+	radius := 5
+	
+	// 3- Calculate the circle circumference (2*PI*radius) and store it in a new variable.
+	calculatedCircle := 2 * pi * float64(radius)
+	
+	// Output the result in the command line
+	fmt.Println(calculatedCircle)
+
+	// 4- Switch to a different way of outputting the result: Format the string 
+	// to say "For a radius of X, the circle circumference is Y.YY"
+	fmt.Printf("For a radius of %v, the circle circumference is %.2f \n", radius, calculatedCircle)
+}
