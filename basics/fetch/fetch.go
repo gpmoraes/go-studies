acaaf06cb92e69f8954a31bea81c0b80ac76ffa5
Guilherme Moraes <moraes.guilhermep@gmail.com>
2023-07-02 21:10:59 -0300

    Now using a file to store the response, the code checks if there is a prefix 'http://', shows the HTTP response status

--- basics/fetch/fetch.go
+++ basics/fetch/fetch.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
-	//urls := []string{"https://example.com", "https://google.com", "https://github.com"}
-	urls := []string{"https://google.com"}
+	urls := []string{"google.com"}
 
-	// Iterate over the command-line arguments starting from index 1
-	// Each argument represents a URL to fetch
+	// Iterate over the URLs to fetch
 	for _, url := range urls {
+
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
 		// Send an HTTP GET request to the URL
 		resp, err := http.Get(url)
 		if err != nil {
@@ -21,15 +25,39 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		// Read the response body into a byte slice
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+
+		fmt.Printf("HTTP Requisition Status: %v\n", resp.Status)
+		defer resp.Body.Close()
+
+		file, err := os.Create("outputBuffer.txt")
+		if err != nil {
+			// If an error occurs while creating the file, print the error message and exit
+			fmt.Fprintf(os.Stderr, "fetch: creating file: %v\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		// Read the response body and write it to the file
+		_, err = io.Copy(file, resp.Body)
 		if err != nil {
 			// If an error occurs while reading the response body, print the error message and exit
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		// Print the response body to the standard output
-		fmt.Printf("%s\n", b)
+
+		fileInfo, err := os.Stat("outputBuffer.txt")
+		if err != nil {
+			// If an error occurs while getting file info, print the error message and exit
+			fmt.Fprintf(os.Stderr, "fetch: getting file info: %v\n", err)
+			os.Exit(1)
+		}
+
+		// Print the file information
+		fmt.Println("========= File Information =========")
+		fmt.Printf("Name: %s\n", fileInfo.Name())
+		fmt.Printf("Size: %d\n", fileInfo.Size())
+		fmt.Printf("Permissions: %s\n", fileInfo.Mode())
+		fmt.Printf("Last Modified: %v\n", fileInfo.ModTime())
+		fmt.Printf("Is Directory: %t\n", fileInfo.IsDir())
 	}
 }
